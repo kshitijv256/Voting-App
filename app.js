d8d37a67973b23823a4fa26d113d7f110c1198f3
Kshitij Verma <kshitijv250@gmail.com>
2022-12-22 17:34:23 +0530

    Added ejs and tailwind

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 app.get('/', (req, res) => {
-    res.send('My Voting App')
+    res.render('index');
     });
 
 module.exports = app;
