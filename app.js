0eb40654c27d0bb83031189370669fac9d4abc53
Kshitij Verma <kshitijv250@gmail.com>
2022-12-24 21:58:02 +0530

    Added some important packages

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
