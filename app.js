9f4543f448e71c0d6be0286cb44f375b633c9c1f
Kshitij Verma <kshitijv250@gmail.com>
2022-12-26 18:27:50 +0530

    properly formatted

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
-const express = require('express');
-const { Election, Question, Answer } = require('./models');
+// imports
+
+const express = require("express");
+const { Election, Question, Answer } = require("./models");
 const path = require("path");
 const bodyParser = require("body-parser");
-
 const app = express();
+
+//==================================================
+
+// middleware
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+//==================================================
 
-app.get('/', (req, res) => {
-    res.render('index');
+// get requests
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+//==================================================
+
+// post requests
+
+app.post("/elections", (req, res) => {
+  console.log(req.body);
+  try {
+    Election.create({
+      title: req.body.title,
+      description: req.body.description,
     });
+    res.send("Election created");
+  } catch (error) {
+    console.log(error);
+    res.send("Error creating election");
+  }
+});
 
-
-app.post('/elections', (req, res) => {
-    console.log(req.body);
-    try {
-        Election.create({
-            title: req.body.title,
-            description: req.body.description,
-        });
-        res.send('Election created');
-    } catch (error) {
-        console.log(error);
-        res.send('Error creating election');
-    }
-    });
+//==================================================
 
 module.exports = app;
