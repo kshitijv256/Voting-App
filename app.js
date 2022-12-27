855e8228f9388ad70a3d3d0793230e8ed8ab2900
Kshitij Verma <kshitijv250@gmail.com>
2022-12-27 22:47:46 +0530

    added feature to add questions in a ballot

--- app.js
+++ app.js
@@ -31,14 +31,34 @@ app.get("/elections", async (req, res) => {
   const elections = await Election.findAll();
   res.render("elections", { elections });
 });
+
+app.get("/elections/:id", async (req, res) => {
+  const election = await Election.findByPk(req.params.id);
+  const questions = await Question.findAll({
+    where: { electionId: req.params.id },
+  });
+  res.render("ballot", { election, questions });
+});
+
+app.get("/questions/add/:electionId", (req, res) => {
+  res.render("edit_question", { electionId: req.params.electionId });
+});
+
+app.get("/questions/edit/:id", async (req, res) => {
+  const question = await Question.findByPk(req.params.id);
+  const answers = await Answer.findAll({
+    where: { questionId: req.params.id },
+  });
+  res.render("edit_question", { question, answers });
+});
 //==================================================
 
 // post requests
 
-app.post("/elections", (req, res) => {
+app.post("/elections", async (req, res) => {
   console.log(req.body);
   try {
-    Election.create({
+    await Election.create({
       title: req.body.title,
       description: req.body.description,
     });
@@ -49,6 +69,21 @@ app.post("/elections", (req, res) => {
   }
 });
 
+app.post("/questions", async (req, res) => {
+  console.log(req.body);
+  try {
+    await Question.create({
+      body: req.body.body,
+      selected: null,
+      correct: null,
+      electionId: req.body.electionId,
+    });
+    res.redirect(`/elections/${req.body.electionId}`);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 //==================================================
 
 module.exports = app;
