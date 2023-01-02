0e1646de2e3d65705e8bde307acec0713655b4ca
Kshitij Verma <kshitijv250@gmail.com>
2023-01-02 23:32:14 +0530

    Added csrf token

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 // imports
 
 const express = require("express");
+const csrf = require("csurf"); // using csrf
+const cookieParser = require("cookie-parser");
+const passport = require("passport"); // using passport
+const LocalStrategy = require("passport-local"); // using passport-local as strategy
+const session = require("express-session");
+// const connectEnsureLogin = require("connect-ensure-login");
+const bcrypt = require("bcrypt");
+// const saltRounds = 10;
+const flash = require("connect-flash");
 // eslint-disable-next-line no-unused-vars
 const { Election, Question, Answer } = require("./models");
 const path = require("path");
@@ -14,17 +23,87 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(flash());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cookieParser("some other secret string"));
+// ["POST", "PUT", "DELETE"]));
+app.use(csrf({ cookie: true }));
+
+app.use(
+  session({
+    secret: "secret-key-that-no-one-can-guess",
+    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 24 hours
+  })
+);
+
+// passport config
+app.use(passport.initialize());
+app.use(passport.session());
+
+// authentication
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (username, password, done) => {
+      User.findOne({
+        where: {
+          email: username,
+        },
+      })
+        .then(async (user) => {
+          const result = await bcrypt.compare(password, user.password);
+          if (result) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: "Incorrect password" });
+          }
+        })
+        .catch(() => {
+          return done(null, false, { message: "User does not exists" });
+        });
+    }
+  )
+);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => {
+      done(err, null);
+    });
+});
+
+app.use(function (request, response, next) {
+  response.locals.messages = request.flash();
+  next();
+});
+
 //==================================================
 
 // get requests
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async function (request, response) {
+  if (request.user) {
+    return response.render("index", {
+      csrfToken: request.csrfToken(),
+    });
+  } else {
+    return response.render("landing", {
+      csrfToken: request.csrfToken(),
+    });
+  }
 });
 
 app.get("/elections", async (req, res) => {
